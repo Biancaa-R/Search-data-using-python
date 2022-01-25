25ee53002c226dc40a5ab523f2e9d5fe6e4d8521
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2022-01-25 15:03:37 +0530

    Update Search data using python.py

--- Search data using python.py
+++ Search data using python.py
@@ -3,13 +3,12 @@ import pywhatkit
 searchvalue=input("Enter the text to search about it")
 try:
     value=pywhatkit.info(searchvalue,10)
-    print(value)
     print("Successfully searched")
     ch=input("Do you want to save this data?y/n")
     if ch in ("y","Y"):
         F=open("D:\\12th File handle\\Information.txt ","a")
-        F.write(searchvalue,"\n")
-        F.write(value,"\n")
+        F.write(searchvalue+"\n")
+        F.write(value+"\n")
         F.close()
         print("Data saved successfully")
 except:
